668126aa9da4fee91008b5fda7eca97a53654639
lang xia <xialang2012@163.com>
2019-01-18 11:04:59 -0500

    add sequence search

---
+++ AlgorithmDataStruct/Finding.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "stdafx.h"
+#include "ADTTree.h"
+
+template<typename T>
+class SeqSearch
+{
+public:
+	SeqSearch();
+	~SeqSearch();
+
+private:
+
+};
+
+template<typename T>
+SeqSearch::SeqSearch()
+{
+}
+
+template<typename T>
+SeqSearch::~SeqSearch()
+{
+}
